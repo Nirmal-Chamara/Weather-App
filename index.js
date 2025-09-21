eff5b2f987135b28264ea6b9180152a2c03a5955
Nirmal Chamara <kpnirmalchamara@gmail.com>
2025-09-21 13:54:22 +0530

    Add files via upload
    A simple weather web app built with HTML, CSS, and JavaScript that fetches real-time weather data from the OpenWeather API.
    ✨ Features:
    -Search for the weather by city name
    -Displays temperature, humidity, description, and weather icon
    -Liquid glass (frosted glass) UI card with animated effects
    -Dynamic background tint/glow based on weather conditions

---
+++ index.js
@@ -0,0 +1,137 @@
+//weather App
+
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "fb4760768a7b27317b49cb7714706eea";
+
+weatherForm.addEventListener("submit", async event => {
+
+    event.preventDefault();
+
+    const city = cityInput.value;
+
+    if(city){
+        try{
+            const weatherData = await getWeatherData(city);
+            displayWeatherInfo(weatherData);
+        }
+        catch(error){
+            console.error(error);
+            displayError(error);
+        }
+
+    }
+    else{
+        displayError("Please Enter a City");
+    }
+
+});
+
+async function getWeatherData(city) {
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    const response = await fetch(apiUrl);
+    //console.log(response);
+
+    if(!response.ok){
+        throw new Error("Could not fetch weather data");
+    }
+    
+    return await response.json();
+}
+
+function displayWeatherInfo(data){
+
+    const {name: city, 
+           main:{temp, humidity},
+           weather:[{description, id}]} = data; //objects in API data
+
+    card.textContent = "";
+    card.style.display = "flex";
+
+    const cityDisplay = document.createElement("h1");
+    const tempDisplay = document.createElement("p");
+    const humidityDisplay = document.createElement("p");
+    const descDisplay = document.createElement("p");
+    const weatherEmoji = document.createElement("p");
+
+    cityDisplay.textContent = city;
+    tempDisplay.textContent = `${(temp - 273.15).toFixed(1)}°C`;
+    humidityDisplay.textContent = `Humidity: ${humidity}%`;
+    descDisplay.textContent = description;
+    weatherEmoji.textContent = getWeatherEmoji(id);
+
+    cityDisplay.classList.add("cityDisplay");
+    tempDisplay.classList.add("tempDisplay");
+    humidityDisplay.classList.add("humidityDisplay");
+    descDisplay.classList.add("descDisplay");
+    weatherEmoji.classList.add("weatherEmoji");
+    
+
+
+
+    card.appendChild(cityDisplay);
+    card.appendChild(tempDisplay);
+    card.appendChild(humidityDisplay);
+    card.appendChild(descDisplay);
+    card.appendChild(weatherEmoji);
+
+    
+    // Reset old weather backgrounds but keep "card"
+    card.classList.remove("sunny", "rainy", "snowy", "cloudy");
+
+    // Change background based on weather ID
+    if(id === 800){
+        card.classList.add("sunny");
+    }
+    else if(id >= 200 && id < 600){
+        card.classList.add("rainy");
+    }
+    else if(id >= 600 && id < 700){
+        card.classList.add("snowy");
+    }
+    else {
+        card.classList.add("cloudy");
+    }
+    
+    card.classList.add("show"); 
+
+}
+
+function getWeatherEmoji(weatherId){
+    switch(true){
+        case (weatherId >= 200 && weatherId < 300):
+            return "⛈️";
+        case (weatherId >= 300 && weatherId < 400):
+            return "🌧️";
+        case (weatherId >= 500 && weatherId < 600):
+            return "🌧️";
+        case (weatherId >= 600 && weatherId < 700):
+            return "🌨️";
+        case (weatherId >= 700 && weatherId < 800):
+            return "🌫️";
+        case (weatherId === 800):
+            return "☀️";
+        case (weatherId >= 801 && weatherId < 810):
+            return "☁️";
+        default:
+            return "❓";
+    }
+}
+
+function displayError(message){
+
+    const errorDisplay = document.createElement("p");
+    errorDisplay.textContent = message;
+    errorDisplay.classList.add("errorDisplay")//last errorDisplay is css class
+
+    card.textContent = "";
+    card.style.display = "flex";
+    card.appendChild(errorDisplay);
+    card.classList.add("show");
+
+
+
+}
